aab4e9a1af0ad793b2a6fbf51f95e0fa10d0a2e2
Sunny Lin <slin@whtrading.com>
2025-07-23 11:45:48 -0500

    started work on creating an algo interface for each one

---
+++ NinjaApiClient/ninja_api_client/algo_interface.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+
+class Algo(ABC):
+    @abstractmethod
+    def __init__():
+        pass
+
+    @abstractmethod
+    def warmup():
+        pass
+
+    @abstractmethod
+    def run():
+        pass
